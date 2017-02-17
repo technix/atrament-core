009561879f662626871601891171c240bc47139b
techniX <sergei.mozhaisky@gmail.com>
2017-02-17 21:34:18 +0200

    Add style to UI

--- src/theme/default/index.js
+++ src/theme/default/index.js
@@ -1,30 +1,42 @@
 // default UI
+require('./style.css');
 var $ = require('jquery');
+
+var config = {
+    scrollMode: 'alwaysTop' // alwaysBottom
+};
 
 var UI = {
     init: function init(choiceCallback) {
-        $('body').append('<div id="text"></div><div id="choices"></div>');
-        $('#choices').on('click', 'a', function choiceSelected(e) {
+        $('#atrament-container').append('<div id="text-container"></div>');
+        $('#atrament-container').append('<div id="choice-container"></div>');
+        $('#choice-container').on('click', 'a', function choiceSelected(e) {
             e.preventDefault();
-            $('#choices').html('');
+            $('#choice-container').html('');
             var id = $(this).attr('data-id');
             choiceCallback(id);
         });
     },
     renderScene: function renderScene(scene) {
-        var storytext = $();
+        var storytext = $('<div class="text-wrapper">');
         scene.text.forEach(function renderText(paragraph) {
-            storytext = storytext.add($('<p>').html(paragraph));
+            storytext = storytext.append($('<p>').html(paragraph));
         });
-        $('#text').append(storytext);
+        $('#text-container').append(storytext);
 
         var choices = $();
         scene.choices.forEach(function renderChoice(choice) {
             choices = choices.add(
-                $('<p>').html('<a href="#" class="choice" data-id="' +  choice.id + '">' + choice.text + '</a>')
+                $('<div class="choice-wrapper">').html(
+                    '<a href="#" class="choice" data-id="' +  choice.id + '">' + choice.text + '</a>'
+                )
             );
         });
-        $('#choices').append(choices);
+        $('#choice-container').append(choices);
+        var scrollHeight = (config.scrollMode === 'alwaysBottom') ?
+            $('#atrament-container').prop('scrollHeight') :
+            storytext.prop('offsetTop');
+        $('#atrament-container').animate({scrollTop: scrollHeight}, 1000);
     }
 };
 

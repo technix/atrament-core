56dbb5198029b1f492cfcb0ac4ff1ac0d0bf81fa
techniX <sergei.mozhaisky@gmail.com>
2017-06-07 17:16:27 +0300

    save/load support

--- src/atrament/index.js
+++ src/atrament/index.js
@@ -1,12 +1,18 @@
+/* global window */
 import AtramentStory from './story';
 
 class Atrament {
-    constructor(storyContent) {
-        this.atramentStory = new AtramentStory(storyContent);
+    constructor(gameId) {
+        this.storage = window.localStorage;
+        this.gameId = gameId;
     }
 
     set ui(uiObj) {
         this.UI = uiObj;
+    }
+
+    set story(storyContent) {
+        this.atramentStory = new AtramentStory(storyContent);
     }
 
     startGame() {
@@ -24,6 +30,16 @@ class Atrament {
         this.UI.renderScene(scene);
     }
 
+    save() {
+        const state = this.atramentStory.saveState();
+        this.storage.setItem(this.gameId, JSON.stringify(state));
+    }
+
+    load() {
+        const state = JSON.parse(this.storage.getItem(this.gameId));
+        this.atramentStory.loadState(state);
+        this.continueStory();
+    }
 }
 
 export default Atrament;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-/* global document */
+/* global document,window */
 import $ from 'jquery';
 import Atrament from './atrament';
 import UI from './theme/paper';
 
+const atrament = new Atrament('intercept');
+window.Atrament = atrament;
+
 $(document).ready(() => {
     $.get('/intercept.ink.json').then((content) => {
-        const atrament = new Atrament(content);
+        atrament.story = content;
         atrament.ui = UI;
         atrament.startGame();
     });

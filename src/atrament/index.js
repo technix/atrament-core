13d5925500643de32759a838ac519cb2ae8dcf12
techniX <sergei.mozhaisky@gmail.com>
2017-07-19 12:01:44 +0300

    unique scene ID, fix scene reset

--- src/atrament/index.js
+++ src/atrament/index.js
@@ -4,6 +4,7 @@ import AtramentStory from './story';
 class Atrament {
     constructor() {
         this.storage = window.localStorage;
+        this.sceneId = 0;
     }
 
     initStory(gameId, storyContent) {
@@ -17,31 +18,40 @@ class Atrament {
     }
 
     startStory() {
-        this.$story = [];
+        this.$story.splice(0);
         return this.updateStory();
     }
 
     continueStory(sceneId, choiceId) {
-        this.$story[sceneId].isActive = false;
+        this.$story.forEach((item) => {
+            if (item.id === sceneId) {
+                item.isActive = false;
+            }
+        });
         this.atramentStory.makeChoice(choiceId);
         return this.updateStory();
     }
 
     updateStory() {
         const scene = this.nextScene();
+        if (scene.type !== 'text') {
+            this.resetStory();
+        }
         this.$story.push(scene);
         return this.$story;
     }
 
     nextScene() {
         const scene = this.atramentStory.getScene();
-        scene.id = this.$story.length;
+        scene.id = this.sceneId;
+        this.sceneId += 1;
         scene.isActive = true;
+        console.log(scene);
         return scene;
     }
 
     resetStory() {
-        this.$story = [];
+        this.$story.splice(0);
     }
 
     save() {

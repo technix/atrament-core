2bd230aaa8295144044e6e552c229c914b9cb1cf
Serhii Mozhaiskyi <sergei.mozhaisky@gmail.com>
2024-08-05 17:22:20 +0300

    cleaner hashCode function

--- src/utils/hashcode.js
+++ src/utils/hashcode.js
@@ -1,11 +1,5 @@
 /* eslint no-bitwise: "off" */
 
 export default function hashCode(str) {
-  let hash = 0;
-  for (let i = 0, len = str.length; i < len; i += 1) {
-    const chr = str.charCodeAt(i);
-    hash = (hash << 5) - hash + chr;
-    hash |= 0; // Convert to 32bit integer
-  }
-  return hash;
+  return [...str].reduce((s, c) => Math.imul(31, s) + c.charCodeAt(0) | 0, 0);
 }

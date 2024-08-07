97b12b55cf752c1c68b42ef84b0cc9a06aadbae0
Serhii Mozhaiskyi <sergei.mozhaisky@gmail.com>
2024-08-07 14:46:04 +0300

    save management: change key handling, use session prefix

--- __tests__/components/saves.test.js
+++ __tests__/components/saves.test.js
@@ -4,7 +4,7 @@ import mockState from '../../__mocks__/state';
 
 import { emit } from '../../src/utils/emitter';
 import ink from '../../src/components/ink';
-import { $getSlotName, load, save, existSave, removeSave, listSaves } from '../../src/components/saves';
+import { getSaveSlotKey, load, save, existSave, removeSave, listSaves } from '../../src/components/saves';
 
 jest.mock('../../src/utils/emitter', () => ({
   emit: jest.fn()
@@ -38,14 +38,24 @@ beforeEach(() => {
 });
 
 describe('components/saves', () => {
-  describe('$getSlotName', () => {
-    test('getSlotName - default', () => {
-      const slotName = $getSlotName();
-      expect(slotName).toBe('test-game/_autosave_');
+  describe('getSaveSlotKey', () => {
+    test('default', () => {
+      const slotName = getSaveSlotKey({ type: 'autosave' });
+      expect(slotName).toBe('test-game//save/autosave/');
     });
-    test('getSlotName - default', () => {
-      const slotName = $getSlotName('save_id');
-      expect(slotName).toBe('test-game/save_id');
+    test('default - named', () => {
+      const slotName = getSaveSlotKey({ type: 'checkpoint', name: 'stage1' });
+      expect(slotName).toBe('test-game//save/checkpoint/stage1');
+    });
+    test('session', () => {
+      mockState.setSubkey('game', '$sessionID', 's1');
+      const slotName = getSaveSlotKey({ type: 'autosave' });
+      expect(slotName).toBe('test-game/s1/save/autosave/');
+    });
+    test('session - named', () => {
+      mockState.setSubkey('game', '$sessionID', 's1');
+      const slotName = getSaveSlotKey({ type: 'checkpoint', name: 'stage1' });
+      expect(slotName).toBe('test-game/s1/save/checkpoint/stage1');
     });
   });
 
@@ -53,12 +63,17 @@ describe('components/saves', () => {
     test('save game state', async () => {
       const mockScenes = ['scene1', 'scene2'];
       mockState.setKey('scenes', mockScenes);
-      const saveID = 'save_id';
-      await save(saveID);
+      const type = 'checkpoint';
+      const name = 'stage2';
+      // run
+      await save({ type, name });
+      // check
       expect(ink.getState).toHaveBeenCalledTimes(1);
-      const saved = await mockPersistent.get($getSlotName(saveID));
+      const saveID = getSaveSlotKey({ type, name });
+      const saved = await mockPersistent.get(saveID);
       expect(saved).toEqual({
-        id: saveID,
+        name,
+        type,
         date: 1691539200000,
         game: mockState.get().game,
         scenes: mockScenes,
@@ -73,10 +88,13 @@ describe('components/saves', () => {
     test('load game state', async () => {
       const mockScenes = ['scene1', 'scene2'];
       mockState.setKey('scenes', mockScenes);
-      const saveID = 'save_id';
-      await save(saveID);
+      const type = 'checkpoint';
+      const name = 'stage2';
+      // run
+      await save({ type, name });
       emit.mockClear();
       // load
+      const saveID = getSaveSlotKey({ type, name });
       await load(saveID);
       expect(ink.loadState).toHaveBeenCalledWith({ inkjson: 'content' });
       expect(mockState.get().scenes).toEqual(mockScenes);
@@ -88,10 +106,13 @@ describe('components/saves', () => {
 
   describe('exist', () => {
     test('game state exists', async () => {
-      const saveID = 'save_id';
+      const type = 'checkpoint';
+      const name = 'stage2';
+      const saveID = getSaveSlotKey({ type, name });
+      // check
       let saveExists = await existSave(saveID);
       expect(saveExists).toBe(false);
-      await save(saveID);
+      await save({ name, type });
       saveExists = await existSave(saveID);
       expect(saveExists).toBe(true);
       await removeSave(saveID);
@@ -103,20 +124,22 @@ describe('components/saves', () => {
   describe('listSaves', () => {
     test('get saves for specific game', async () => {
       mockState.setSubkey('game', 'gameUUID', 'UUID1');
-      await save('game1_save1');
-      await save('game1_save2');
-      await save('game1_autosave');
+      await save({ type: 'save', name: 'game1_save1' });
+      await save({ type: 'checkpoint', name: 'game1_save2' });
+      await save({ type: 'autosave' });
       let savesList = await listSaves();
       expect(savesList).toEqual([{
-        id: 'game1_save1',
+        name: 'game1_save1',
+        type: 'save',
         game: { gameUUID: 'UUID1' },
         date: 1691539200000
       }, {
-        id: 'game1_save2',
+        name: 'game1_save2',
+        type: 'checkpoint',
         game: { gameUUID: 'UUID1' },
         date: 1691539200000
       }, {
-        id: 'game1_autosave',
+        type: 'autosave',
         game: { gameUUID: 'UUID1' },
         date: 1691539200000
       }]);
@@ -126,10 +149,11 @@ describe('components/saves', () => {
       expect(savesList).toEqual([]);
       expect(emit).toHaveBeenCalledWith('game/listSaves', []);
       // save something under new UUID
-      await save('game2_checkpoint');
+      await save({ type: 'checkpoint', name: 'game2_checkpoint' });
       savesList = await listSaves();
       expect(savesList).toEqual([{
-        id: 'game2_checkpoint',
+        name: 'game2_checkpoint',
+        type: 'checkpoint',
         game: { gameUUID: 'UUID2' },
         date: 1691539200000
       }]);
@@ -138,15 +162,17 @@ describe('components/saves', () => {
       mockState.setSubkey('game', 'gameUUID', 'UUID1');
       savesList = await listSaves();
       expect(savesList).toEqual([{
-        id: 'game1_save1',
+        name: 'game1_save1',
+        type: 'save',
         game: { gameUUID: 'UUID1' },
         date: 1691539200000
       }, {
-        id: 'game1_save2',
+        name: 'game1_save2',
+        type: 'checkpoint',
         game: { gameUUID: 'UUID1' },
         date: 1691539200000
       }, {
-        id: 'game1_autosave',
+        type: 'autosave',
         game: { gameUUID: 'UUID1' },
         date: 1691539200000
       }]);
--- src/components/saves.js
+++ src/components/saves.js
@@ -3,63 +3,75 @@ import { interfaces } from '../utils/interfaces';
 import { emit } from '../utils/emitter';
 
 
-export function $getSlotName(id) {
-  const { gameUUID } = interfaces().state.get().game;
-  let saveId = id;
-  if (!id) {
-    saveId = '_autosave_';
-  }
-  return `${gameUUID}/${saveId}`;
+function savePrefix() {
+  const { gameUUID, $sessionID } = interfaces().state.get().game;
+  return [
+    gameUUID,
+    $sessionID || '',
+    'save'
+  ].join('/');
 }
 
 
-export async function load(saveID) {
+export function getSaveSlotKey({ name, type }) {
+  return [
+    savePrefix(),
+    type || '',
+    name
+  ].join('/');
+}
+
+
+export async function load(saveSlotKey) {
   const { persistent, state } = interfaces();
-  const gameState = await persistent.get($getSlotName(saveID));
+  const gameState = await persistent.get(saveSlotKey);
   state.setKey('scenes', gameState.scenes);
   state.setKey('game', gameState.game);
   ink.loadState(gameState.state);
-  emit('game/load', saveID);
+  emit('game/load', saveSlotKey);
 }
 
 
-export async function save(saveID) {
+export async function save({ name, type }) {
   const { state, persistent } = interfaces();
   const atramentState = state.get();
   const gameState = {
-    id: saveID,
+    name,
+    type,
     date: Date.now(),
     state: ink.getState(),
     game: atramentState.game,
     scenes: atramentState.scenes
   };
-  await persistent.set($getSlotName(saveID), gameState);
-  emit('game/save', saveID);
+  const saveSlotKey = getSaveSlotKey({ name, type });
+  await persistent.set(saveSlotKey, gameState);
+  emit('game/save', saveSlotKey);
 }
 
 
-export async function existSave(saveID) {
-  const saveExists = await interfaces().persistent.exists($getSlotName(saveID));
+export async function existSave(saveSlotKey) {
+  const saveExists = await interfaces().persistent.exists(saveSlotKey);
   return saveExists;
 }
 
 
-export async function removeSave(saveID) {
-  await interfaces().persistent.remove($getSlotName(saveID));
-  emit('game/removeSave', saveID);
+export async function removeSave(saveSlotKey) {
+  await interfaces().persistent.remove(saveSlotKey);
+  emit('game/removeSave', saveSlotKey);
 }
 
 
 export async function listSaves() {
-  const { state, persistent } = interfaces();
-  const { gameUUID } = state.get().game;
+  const { persistent } = interfaces();
   const keys = await persistent.keys();
-  const saves = keys.filter((k) => k.includes(gameUUID));
+  const saves = keys.filter((k) => k.includes(savePrefix()));
   const savesList = await Promise.all(
     saves.map(
-      async (saveID) => {
-        const { id, date, game } = await persistent.get(saveID);
-        return { id, date, game };
+      async (key) => {
+        const saveData = await persistent.get(key);
+        delete saveData.state;
+        delete saveData.scenes;
+        return saveData;
       }
     )
   );

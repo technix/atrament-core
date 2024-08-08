8521f0f50577b6065eea99e090dc059a7d3576e9
Serhii Mozhaiskyi <sergei.mozhaisky@gmail.com>
2024-08-08 11:49:20 +0300

    version bump

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import ink from './components/ink';
 import settings from './components/settings';
 
 // @atrament/core version
-const version = '1.3.0';
+const version = '2.0.0';
 
 /*
 Initialize engine:
